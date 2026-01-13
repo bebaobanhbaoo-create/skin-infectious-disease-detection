f1226c076ac1bf22fe90cadb2b6a3f8b6fe48c0b
Mai Dong Anh <bebaobanhbaoo-create@users.noreply.github.com>
2026-01-13 12:35:34 +0700

    feat(utils): add best checkpoint finder

--- src/utils/checkpoint.py
+++ src/utils/checkpoint.py
@@ -14,3 +14,6 @@ def load_checkpoint(path, model, optimizer=None):
     if optimizer:
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     return checkpoint['epoch']
+
+def get_best_checkpoint(checkpoint_dir):
+    pass
