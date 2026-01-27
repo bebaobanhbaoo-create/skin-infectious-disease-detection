eb5582052c4137675d6239f2ca7204304ab8066b
Mai Dong Anh <bebaobanhbaoo-create@users.noreply.github.com>
2026-01-27 12:35:34 +0700

    feat(models): add backbone listing

--- src/models/backbone.py
+++ src/models/backbone.py
@@ -10,3 +10,6 @@ def get_backbone_output_dim(model_name='resnet50'):
     """Get output dimension of backbone."""
     model = timm.create_model(model_name, pretrained=False, num_classes=0)
     return model.num_features
+
+def list_available_backbones():
+    return ['resnet18', 'resnet50', 'efficientnet_b0']
