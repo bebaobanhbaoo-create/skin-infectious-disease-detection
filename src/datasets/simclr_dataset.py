a7fe04ade13ce834afd510dd306466e7e15f4e96
Mai Dong Anh <bebaobanhbaoo-create@users.noreply.github.com>
2026-06-17 12:42:24 +0700

    feat(data): enhance SimCLR dataset with utilities and statistics
    - Add informative __repr__ for debugging
    - Implement create_subset() for data splitting
    - Add get_statistics() for normalization computation
    - Support dynamic transform updates

--- src/datasets/simclr_dataset.py
+++ src/datasets/simclr_dataset.py
@@ -23,3 +23,39 @@ class SimCLRDataset(Dataset):
 
     def set_transform(self, transform):
         self.transform = transform
+
+    def __repr__(self):
+        """Return informative string representation."""
+        return f'SimCLRDataset(size={len(self)}, transform={self.transform is not None})'
+    
+    def set_transform(self, transform):
+        """Update augmentation transform."""
+        self.transform = transform
+    
+    def get_image_paths(self):
+        """Return list of all image paths."""
+        return self.image_paths.copy()
+    
+    def create_subset(self, indices):
+        """Create a subset dataset with given indices."""
+        subset_paths = [self.image_paths[i] for i in indices]
+        return SimCLRDataset(subset_paths, self.transform)
+    
+    def get_statistics(self):
+        """Compute dataset statistics for normalization."""
+        import numpy as np
+        from PIL import Image
+        from tqdm import tqdm
+        
+        means = []
+        stds = []
+        
+        for path in tqdm(self.image_paths[:1000], desc="Computing stats"):
+            img = np.array(Image.open(path).convert('RGB')) / 255.0
+            means.append(img.mean(axis=(0, 1)))
+            stds.append(img.std(axis=(0, 1)))
+        
+        return {
+            'mean': np.mean(means, axis=0).tolist(),
+            'std': np.mean(stds, axis=0).tolist()
+        }
